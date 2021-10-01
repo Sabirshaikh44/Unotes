e824de76995cd379296fc715f26ccf948609f3a0
Shaikh Sabir <89734545+Sabirshaikh44@users.noreply.github.com>
2021-10-01 11:19:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,19 @@
-console.log("Welcome to the project of Unotes , you can do it!");
-/*noteTxt, addBtn ,notes*/
 showNotes();
-// user enter notes here will add it when it will be clicked:
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function (e) {
   let noteTitle = document.getElementById("noteTitle");
   let noteTxt = document.getElementById("noteTxt");
-  // getting notes from localstorage:
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
-    //JSON.Parse use to convert the coming values from any web or user input into the object.
     notesObj = JSON.parse(notes);
   }
   myObj = {
     noteTitle: noteTitle.value,
     noteTxt: noteTxt.value,
   };
-  // check if user input something then only create a note card else inform to write something:
   if (noteTxt.value && noteTitle.value != 0) {
-    //Push the value user entered into the notesObj[] object:
     notesObj.push(myObj);
     noteTxt.classList.remove("border-danger");
     noteTitle.classList.remove("border-danger");
@@ -30,24 +23,17 @@ addBtn.addEventListener("click", function (e) {
     noteTitle.classList.add("border-danger");
     noteTxt.placeholder = "please enter something";
   }
-
-  //Updating the local storage and setting the value their:
-  //to converts that coming data-which is in object into the string.
   localStorage.setItem("notes", JSON.stringify(notesObj));
-  // value will be resetted into box,so previously entered value will not appear into the textarea again:
   noteTitle.value = "";
   noteTxt.value = "";
   showNotes();
 });
 
-//Function to add inputnote and show notes into the cards:
 function showNotes() {
-  // getting notes from localstorage:
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
-    //JSON.Parse use to convert the coming values from any web or user input into the object.
     notesObj = JSON.parse(notes);
   }
   let card = "";
@@ -69,24 +55,19 @@ function showNotes() {
   }
 }
 
-// fucntion to delete a note:
 function deleteNote(index) {
   let noteTxt = document.getElementById("noteTxt");
-  // getting notes from localstorage:
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
-    //JSON.Parse use to convert the coming values from any web or user input into the object.
     notesObj = JSON.parse(notes);
   }
-  // this will delete the note by index:
   notesObj.splice(index, 1);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   showNotes();
 }
 
-// Search functionality to find note:
 let searchBox = document.getElementById("searchBox");
 searchBox.addEventListener("input", function (e) {
   let searchValue = searchBox.value.toLowerCase();
